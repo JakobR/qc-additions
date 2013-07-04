969146a835494fe2d0d79cf7bd90f50f44a6ef01
Jakob Rath <git@jakobrath.eu>
2013-07-04 17:36:47 +0200

    Convert args to text type for comparison.
    This is necessary as PostgreSQL 9.2 can't perform comparisons on the
    JSON data type, which queue_classic now uses as default for the args
    column.

--- lib/qc-additions/queries.rb
+++ lib/qc-additions/queries.rb
@@ -2,14 +2,18 @@ module QC
   module Queries
     extend self
 
+    # TODO:
+    # Once PostgreSQL supports JSON comparison, use it.
+    # But older versions should be supported too (at least >= 9.0).
+
     def job_count(q_name, method, args)
-      s = "SELECT COUNT(*) FROM #{TABLE_NAME} WHERE q_name = $1 AND method = $2 AND args = $3 AND locked_at IS NULL"
+      s = "SELECT COUNT(*) FROM #{TABLE_NAME} WHERE q_name = $1 AND method = $2 AND args::text = $3 AND locked_at IS NULL"
       r = Conn.execute(s, q_name, method, JSON.dump(args))
       r["count"].to_i
     end
 
     def job_exists?(q_name, method, args)
-      s = "SELECT 1 AS one FROM #{TABLE_NAME} WHERE q_name = $1 AND method = $2 AND args = $3 AND locked_at IS NULL LIMIT 1"
+      s = "SELECT 1 AS one FROM #{TABLE_NAME} WHERE q_name = $1 AND method = $2 AND args::text = $3 AND locked_at IS NULL LIMIT 1"
       r = Conn.execute(s, q_name, method, JSON.dump(args))
       !r.nil?
     end
