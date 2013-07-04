653c842f4ee4a162a75e321fab99c023b431401a
Jakob Rath <git@jakobrath.eu>
2013-07-04 17:10:29 +0200

    Use built-in JSON, like queue_classic does.

--- lib/qc-additions/queries.rb
+++ lib/qc-additions/queries.rb
@@ -4,13 +4,13 @@ module QC
 
     def job_count(q_name, method, args)
       s = "SELECT COUNT(*) FROM #{TABLE_NAME} WHERE q_name = $1 AND method = $2 AND args = $3 AND locked_at IS NULL"
-      r = Conn.execute(s, q_name, method, OkJson.encode(args))
+      r = Conn.execute(s, q_name, method, JSON.dump(args))
       r["count"].to_i
     end
 
     def job_exists?(q_name, method, args)
       s = "SELECT 1 AS one FROM #{TABLE_NAME} WHERE q_name = $1 AND method = $2 AND args = $3 AND locked_at IS NULL LIMIT 1"
-      r = Conn.execute(s, q_name, method, OkJson.encode(args))
+      r = Conn.execute(s, q_name, method, JSON.dump(args))
       !r.nil?
     end
 
